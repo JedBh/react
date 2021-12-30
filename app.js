62a93ab0b294069445d31932a16f36538974eeee
JedBh <jbenhod@gmail.com>
2021-12-30 13:28:14 +0200

    project is ready to start learning react

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Server is up");
+});
